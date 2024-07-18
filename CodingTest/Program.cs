b4ef12e6b8267fab4d36f8958a12bdda63a71849
HawoonShin <sinhaoon@gmail.com>
2024-07-18 17:17:12 +0900

    나이출력
    단순 계산

--- CodingTest/Program.cs
+++ CodingTest/Program.cs
@@ -2,17 +2,11 @@
 
 public class Solution
 {
-    public int solution(int num1, int num2)
+    public int solution(int age)
     {
-        int result;
-        if (num1 == num2)
-        {
-            result = 1;
-        }
-        else
-        {
-            result = -1;
-        }
-        return result;
+
+
+        int answer = 2022 - (age - 1);
+        return answer;
     }
 }
