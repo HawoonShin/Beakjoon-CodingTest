fd3549ee4d5c7cf60b42181d5147a7f82e610ebd
HawoonShin <sinhaoon@gmail.com>
2024-07-18 17:15:30 +0900

    숫자 비교하기
    if문을 이용하여 작성

---
+++ CodingTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int num1, int num2)
+    {
+        int result;
+        if (num1 == num2)
+        {
+            result = 1;
+        }
+        else
+        {
+            result = -1;
+        }
+        return result;
+    }
+}
