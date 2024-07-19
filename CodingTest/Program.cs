c08c2d48bf4e98481287acdc3ed12d42721e06f6
HawoonShin <sinhaoon@gmail.com>
2024-07-19 09:09:54 +0900

    두 수의 나눗셈

--- CodingTest/Program.cs
+++ CodingTest/Program.cs
@@ -2,9 +2,10 @@
 
 public class Solution
 {
-    public int solution(int num1, int num2)
+    public int solution(float num1, float num2)
     {
-        int answer = num1 + num2;
+        float result = (num1 / num2) * 1000;
+        int answer = (int)result;
         return answer;
     }
 }
