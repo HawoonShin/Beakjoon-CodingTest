d19d9b39d1da4b270aab68b0ad0a96c218054c7c
HawoonShin <sinhaoon@gmail.com>
2024-08-06 16:19:51 +0900

    B)A+B
    백준
    문제 1000

---
+++ A+B/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace A_B
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 2024년 8월 6일
+        /// 문제 :
+        /// url :https://www.acmicpc.net/problem/1000
+        /// </summary>
+        static void Main(string[] args)
+        {
+            //문자 받기
+            string[] inputs = Console.ReadLine().Split();
+            int.TryParse(inputs[0], out int a );
+            int.TryParse(inputs[1], out int b );
+           
+            //a+b
+            int sum = a+ b;
+
+            //출력하기
+            Console.WriteLine(sum);
+        }
+    }
+}
